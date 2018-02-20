868b9a7f47bd269f6f24d3eac1b6663217b68397
Tran Situ <tsitu13@gmail.com>
2018-02-19 22:16:18 -0800

    formatio
    herro

--- build/processTimestamp.js
+++ build/processTimestamp.js
@@ -20,6 +20,8 @@
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
       executablePath: "google-chrome-beta"
+      // executablePath:
+      //   "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
     });
 
     const page = await browser.newPage();
@@ -29,19 +31,23 @@
     await browser.close();
 
     const $ = cheerio.load(body);
-    const result = $(".css-truncate-target [datetime]")
-      .map((i, el) => $(el).text())
+    const result = $("time-ago")
+      .map((i, el) => $(el).attr("title"))
       .get();
     return result;
   }
 
   fetchTimestamps(
-    // "https://github.com/tsitu/MH-Tools/tree/master/src/" //borked, un-opened folder issue?
+    // "https://github.com/tsitu/MH-Tools/tree/master/src/"
     "https://github.com/tsitu/test-timestamp/tree/master/data"
   ).then(res => {
+    const format = res.map(el => {
+      const spl = el.split(", ");
+      return `${spl[0]}, ${spl[1]}`;
+    });
     const bookmarkletJson = {};
     for (let i = 0; i < bookmarkletList.length; i++) {
-      bookmarkletJson[bookmarkletList[i]] = res[i];
+      bookmarkletJson[bookmarkletList[i]] = format[i];
     }
     fileUtils.saveJsonFile("data/test.json", bookmarkletJson);
   });
