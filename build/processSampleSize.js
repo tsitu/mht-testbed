eb927d01817d7a582fd19b98caa4801c6dbf2027
Tran Situ <trancsitu@gmail.com>
2018-08-25 23:59:05 -0700

    Commence!

--- build/processSampleSize.js
+++ build/processSampleSize.js
@@ -372,9 +372,9 @@ function processDetailed() {
  * Consistent console.log ordering by using separate functions and chaining
  */
 async function calculateDiffs() {
-  // await processOverall();
-  // await processLocation();
-  // await processDetailed();
+  await processOverall();
+  await processLocation();
+  await processDetailed();
 
   // Finally, output JSON to overwrite the 3 initial files
   outputJSON();
